cbcfde74ef73d93742e38783e1851818a6d4ec39
Miloš Ninković <mmd.doo@gmail.com>
2023-04-11 10:02:13 +0200

    Galerija test

--- src/views/Galerija.vue
+++ src/views/Galerija.vue
@@ -1,14 +1,41 @@
 <script>
- 
+
 </script>
 
 <template>
- 
-  </template>
+  <div id="images_wrapper">
+    <img class="image" src="../assets/Privatni/MMDPrivatni01.jpg"/>
+    <img class="image" src="../assets/Privatni/MMDPrivatni02.jpg"/>
+    <img class="image" src="../assets/Privatni/MMDPrivatni03.jpg"/>
+    <img class="image" src="../assets/Privatni/MMDPrivatni04.jpg"/>
+    <img class="image" src="../assets/Privatni/MMDPrivatni05.jpg"/>
+    <img class="image" src="../assets/Privatni/MMDPrivatni06.jpg"/>
+    <img class="image" src="../assets/Privatni/MMDPrivatni07.jpg"/>
+    <img class="image" src="../assets/Privatni/MMDPrivatni08.jpg"/>
+    <img class="image" src="../assets/Privatni/MMDPrivatni09.jpg"/>
+  </div>
+</template>
 
 <style>
-.proba{
-    background-color: aliceblue;
-    font-size:5em;
+body{
+  height:100vh;
+  width: 100vw;
+  background-color: black;
+  margin: 0rem;
+  overflow: hidden;
 }
+  #images_wrapper > .image{
+    width:40vmin;
+    height:56vmin;
+    object-fit: cover;
+    object-position: center;
+  }
+  #images_wrapper{
+    display: flex;
+    gap: 4vmin;
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(0%,-50%);
+  }
 </style>
