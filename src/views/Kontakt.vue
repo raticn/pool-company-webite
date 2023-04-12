f865fea37b209adad15aec5e941a2433b5af8270
Miloš Ninković <mmd.doo@gmail.com>
2023-04-12 14:13:51 +0200

    ispravljena mapa

--- src/views/Kontakt.vue
+++ src/views/Kontakt.vue
@@ -29,7 +29,7 @@ export default {
     <Nav/>
     <div class="kontakt">
         <div class="map_holder">
-            <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d11313.14320641218!2d20.3826455!3d44.8564762!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x475a65d31e4ececf%3A0xda2a3bdf3b438749!2sMMD%20bazeni!5e0!3m2!1ssr!2srs!4v1681041950859!5m2!1ssr!2srs" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d5656.573898576536!2d20.379310247281605!3d44.856452833266246!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x475a65d31e4ececf%3A0xda2a3bdf3b438749!2sMMD%20bazeni!5e0!3m2!1ssr!2srs!4v1681301340047!5m2!1ssr!2srs" width="800" height="600" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
         </div>
         <div class="text_holder">
             <p><FontAwesomeIcon  class="kontaktIcons" icon="fa-solid fa-location-dot"></FontAwesomeIcon>ŠUMADIJSKA 2, ZEMUN , BEOGRAD </p>
